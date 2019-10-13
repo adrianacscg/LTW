c19a514eb2437957b8e651e3a487b64308d61a41
Adriana Gonçalves <adrianacscg98@gmail.com>
2019-10-13 20:05:27 +0100

    Added PHP exercise 1 and 2

---
+++ PHP/sum2.php
@@ -0,0 +1,12 @@
+<?php 
+
+$num1 = $_GET['num1'];
+$num2 = $_GET['num2'];
+$sum = $num1+$num2;
+echo "<h1>The sum of $num1 + $num2 is: $sum</h1>";
+
+echo "<a href=\"form2\"><h2>Go back</h2><a/>";
+
+echo "<img src=\"https://i.pinimg.com/originals/4f/54/29/4f5429df5ea6361fa8d3f08dfcdccdf9.jpg\" height=400px; alt=\"humm\">";
+
+?>
