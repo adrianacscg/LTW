bc3225ff981d8c5877d89c5ee7457443257014ac
Adriana Gonçalves <adrianacscg98@gmail.com>
2019-10-15 12:24:31 +0100

    PHP exercise 3

--- PHP/sum2.php
+++ PHP/sum2.php
@@ -1,12 +1,24 @@
-<?php 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form Result</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
 
-$num1 = $_GET['num1'];
-$num2 = $_GET['num2'];
-$sum = $num1+$num2;
-echo "<h1>The sum of $num1 + $num2 is: $sum</h1>";
+        <?php 
 
-echo "<a href=\"form2\"><h2>Go back</h2><a/>";
+        $num1 = $_GET['num1'];
+        $num2 = $_GET['num2'];
+        $sum = $num1+$num2;
+        echo "<h1>The sum of $num1 + $num2 is: $sum</h1>";
 
-echo "<img src=\"https://i.pinimg.com/originals/4f/54/29/4f5429df5ea6361fa8d3f08dfcdccdf9.jpg\" height=400px; alt=\"humm\">";
+        echo "<a href='form2'><h2>Go back</h2><a/>";
 
-?>
+        echo "<img src='https://i.pinimg.com/originals/4f/54/29/4f5429df5ea6361fa8d3f08dfcdccdf9.jpg' height=400px; alt='humm'>";
+
+        ?>
+
+    </body>
+
+</html>
